0fa23e36a4a51bf35095e9af762f895a8840c036
jonasgeirs <jonasgeirs16@gmail.com>
2024-12-10 15:10:41 +0000

    sort upcoming movies during api call

--- DrCinema/src/api/services/upcomingMovies.js
+++ DrCinema/src/api/services/upcomingMovies.js
@@ -3,6 +3,11 @@ import apiClient from './apiClient';
 export const getUpcomingMovies = async () => {
   try {
     const response = await apiClient.get('/upcoming'); // API endpoint for fetching upcoming movies
+    const sortedResponse = response.data.sort((a, b) => {
+      const dateA = new Date(a['release-dateIS']);
+      const dateB = new Date(b['release-dateIS']);
+      return dateA - dateB
+    });
     return response.data; // Assuming the API returns a `data` object
   } catch (error) {
     console.error('Error fetching upcoming movies:', error);
